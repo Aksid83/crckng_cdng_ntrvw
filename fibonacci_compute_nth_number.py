31233eaf47da89bd62e1e6da7827bf2041cecf3b
Stan <stan@Stans-MacBook-Pro.local>
2018-04-25 18:45:59 -0700

    Task: Return the Nth Fibonacci number using recursion

---
+++ fibonacci_compute_nth_number.py
@@ -0,0 +1,14 @@
+"""
+Return the Nth Fibonacci number using recursion
+"""
+
+
+def fib(num):
+    if num <= 0:
+        return 0
+    elif num == 1:
+        return 1
+    return fib(num - 1) + fib(num - 2)
+
+
+print fib(16)
