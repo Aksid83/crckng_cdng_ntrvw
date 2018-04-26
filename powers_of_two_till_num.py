4bd542d2b09ded3403ab6e6229440d0dcbe51336
Stan <stan@Stans-MacBook-Pro.local>
2018-04-25 19:14:24 -0700

    Task: Print power of 2 from 2 till input number inclusively with recursion

---
+++ powers_of_two_till_num.py
@@ -0,0 +1,19 @@
+"""
+Print power of 2 from 2 till input number inclusively with recursion
+"""
+
+
+def powers_of_two(num):
+    if num < 1:
+        return 0
+    elif num == 1:
+        print 1
+        return 1
+    else:
+        prev = powers_of_two(num / 2)
+        curr = prev * 2
+        print curr
+        return curr
+
+
+powers_of_two(128)
